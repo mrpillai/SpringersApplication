2fc554c0958118e64f039297d4b98d4fe7329f59
mrpillai <pillaimridu@gmail.com>
2019-12-27 06:38:58 +0530

    Application context tested

--- springer.application/src/main/java/com/springer/springer/application/Application.java
+++ springer.application/src/main/java/com/springer/springer/application/Application.java
@@ -2,12 +2,31 @@ package com.springer.springer.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@ImportResource({"classpath*:appBeans/dbBeanContext.xml"})
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		
+		/* String confFile = "appBeans/dbBeanContext.xml";
+	        ConfigurableApplicationContext context 
+	                                = new ClassPathXmlApplicationContext(confFile);
+	       
+	        */
+		
+		 DBConfig dbConfig = (DBConfig) context.getBean("dbConfig");
+	        System.out.println(dbConfig.toString());
+	        
+	        for (String name : context.getBeanDefinitionNames()) {
+				System.out.println("Cont:---"+name);
+			}
+	        context.close();
+		
 	}
 
 }
